88aff4a62fbdf741eea856cc4303c44d48d19f2e
Christine Huynh <s3438653@student.rmit.edu.au>
2019-01-24 20:23:53 +1100

    Modified rainfall page to have 2 dropdowns

--- rainfall.php
+++ rainfall.php
@@ -4,7 +4,26 @@ include_once("nav.html");
 <html>
 <body>
 <div class="main">
-    <h1>Rainfall page</h1>
+    <h2>Rainfall page</h2>
+    <div class="input-group">
+    <label for="type">Type of data:</label>
+              <select class="form-control" id="observation" name="observation">
+                <option hidden >Select an observation</option>
+                <option value="Daily">Daily</option>
+                <option value="Monthly">Monthly</option>
+              </select>          
+            </div>
+            <div class="input-group">
+            <label for="type">Location:</label>
+              <select class="form-control" id="location" name="location">
+                <option hidden >Select a weather station</option>
+                <option value="Melbourne">Melbourne</option>
+                <option value="MelbourneAirport">Melbourne Airport</option>
+                <option value="EastMelbourne">East Melbourne</option>
+                <option value="NorthMelbourne">North Melbourne</option>
+              </select>          
+
+            </form>
 </div>
 </body>
 </html>
