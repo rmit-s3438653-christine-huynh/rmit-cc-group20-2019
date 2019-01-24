cb2e525cb60edaf43f501d08c6f0bf6085ded6f4
Christine Huynh <s3438653@student.rmit.edu.au>
2019-01-24 17:28:06 +1100

    Base pages for Temperature, Rainfall, Climate created. Separated index file into components. Modified css and app.yaml

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
 <?php
-
+include_once("nav.html");
 ?>
 <html>
-    <head>
-        <meta name="viewport" content="text/html; charset=utf-8" http-equiv="Content-Type">
-        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>-->
-        <link rel="stylesheet" type="text/css" href="/css/sidenav.css">
-    </head>
-    <body>
-    <div class="sidenav">
-  <a href="#">BOM Weather App</a>
-  <a href="#services">Temperature</a>
-  <a href="#clients">Rainfall</a>
-  <a href="#contact">Climate</a>
-</div>
 
+<body>
 <div class="main">
   <h3>Lorem Ipsum</h3>
   <p>This sidebar is of full height (100%) and always shown.</p>
---
+++ rainfall.php
@@ -0,0 +1,10 @@
+<?php
+include_once("nav.html");
+?>
+<html>
+<body>
+<div class="main">
+    <h1>Rainfall page</h1>
+</div>
+</body>
+</html>
