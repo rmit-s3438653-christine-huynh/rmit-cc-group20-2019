b1176fe28555a7e4a494117b910fd7e5b354f3a2
Christine Huynh <s3438653@student.rmit.edu.au>
2019-01-27 18:26:10 +1100

    Forgot to add climate.php changes

--- climate.php
+++ climate.php
@@ -4,21 +4,13 @@
 ?>
 
 <html>
-<<<<<<< HEAD
     <body>
         <div class="main">
             <h1>Climate page</h1>
             <div id="c_container"></div>
+            <?php
+                include_once("map.php");
+            ?>
         </div>
     </body>
-=======
-<body>
-<div class="main">
-    <h1>Climate page</h1>
-	<?php
-	include_once("map.php");
-	?>
-</div>
-</body>
->>>>>>> 9b1e4015fa79561ce9731164b5d2c6ff691cb3d1
 </html>
