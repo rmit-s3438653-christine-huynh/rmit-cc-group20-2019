de6487b06290d2a369a5bc1cacc205f4a53a4c34
Christine Huynh <s3438653@student.rmit.edu.au>
2019-02-01 12:58:51 +1100

    Added phpmyadmin functionality - Able to access database but not access table

--- rainfall.php
+++ rainfall.php
@@ -2,7 +2,7 @@
 <?php
   session_start();
   include_once("nav.html");
-  require_once 'php/google-api-php-client/vendor/autoload.php';
+
 ?>
 
 <html>
@@ -25,7 +25,7 @@
               <option>Melbourne Botanic Gardens</option>
             </select>
           </div><!--form-group-->
-
+          
           <div class="form-group">
             <label for="year">Year:</label>
             <select class="form-control-inline" id="year">
@@ -54,44 +54,7 @@
 
 
     <div class='content'>
-    <?php
-		$client = new Google_Client();
-		$client->useApplicationDefaultCredentials();
-		$client->addScope(Google_Service_Bigquery::BIGQUERY);
-		$bigquery = new Google_Service_Bigquery($client);
-		$projectId = 's3438653-cc2019';
 
-		$request = new Google_Service_Bigquery_QueryRequest();
-		$str = '';
-		
-		$request->setQuery("SELECT year, month, day, rainfall FROM [daily_rainfall.bentleigh] LIMIT 10");
-		
-		$response = $bigquery->jobs->query($projectId, $request);
-		$rows = $response->getRows();
-
-		$str = "<table>".
-		"<tr>" .
-		"<th>Year</th>" .
-    "<th>Month</th>" .
-		"<th>Day</th>" .
-    "<th>Rainfall (mm)</th>" .
-		"</tr>";
-		
-		foreach ($rows as $row)
-		{
-			$str .= "<tr>";
-
-			foreach ($row['f'] as $field)
-			{
-				$str .= "<td>" . $field['v'] . "</td>";
-			}
-			$str .= "</tr>";
-		}
-
-		$str .= '</table></div>';
-
-		echo $str;
-	?>
 </div>
     <div id="r_container"></div>
   </div><!--main-->
--- temperature.php
+++ temperature.php
@@ -1,11 +1,70 @@
 <?php
     include_once("nav.html");
+
+    $servername = "/cloudsql/s3438653-cc2019:us-east1:phpmyadmin-id";
+    $username = "root";
+    $password = "password";
+    $dbname = "cloudcomputing";
+    $c = new mysqli(null, $username, $password, $dbname, 0, $servername);
+
+    if ($c->connect_error) {
+        die('Connect Error (' . $c->connect_errno . ') '
+                . $c->connect_error);
+    } else {
+        echo '-----------------------------------------Success. ' . $c->host_info . "\n";
+    }
+    
+    
+    //$c->close();
 ?>
 
 <html>
     <body>
         <div class="main">
             <h1>Temperature page</h1>
+            <?php
+            $query = "SELECT * FROM `monthly_rainfall`";
+
+            if ($result = $c->query("SELECT DATABASE()")) {
+                $row = $result->fetch_row();
+                printf("Default database is %s.\n", $row[0]);
+                $result->close();
+            }
+
+
+            
+            $result = $c->query($query);
+            while($row = mysql_fetch_assoc($result))
+            {
+                $month = $row['Month'];
+                $precip = $row['Monthly Precipitation Total (millimetres)'];
+                $code = $row['Product code'];
+                $quality = $row['Quality'];
+                $station = $row['Station number'];
+                $year = $row['Year'];
+
+                $month = htmlspecialchars($row['Month'],ENT_QUOTES);
+                $precip = htmlspecialchars($row['Monthly Precipitation Total (millimetres)'],ENT_QUOTES);
+                $code = htmlspecialchars($row['Product code'],ENT_QUOTES);
+                $quality = htmlspecialchars($row['Quality'],ENT_QUOTES);
+                $station = htmlspecialchars($row['Station number'],ENT_QUOTES);
+                $year = htmlspecialchars($row['Year'],ENT_QUOTES);
+
+                echo "  <div style='margin:30px 0px;'>
+              
+                    Station: $station<br />
+                    Year: $year<br />
+                    Month: $month<br />
+                    Monthly Precipitation Total (millimetres): $precip<br />
+                  </div>
+                ";
+            }
+            $result->close();
+
+            $c->close();
+
+
+            ?>
             <div id="t_container"></div>
         </div>
     </body>
